abb0c6a9ef6d6b6fe049cb0c4e688fd1f2204028
Meqr1 <ayanmahajan41@gmail.com>
2025-01-12 05:08:03 +0530

    fixed layout

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,6 +1,21 @@
 'use client'
-import { signup } from "@/pages/auth/actions";
 import { Key, useActionState } from "react";
+import instance from "@/lib/axios";
+
+export async function signup(state: unknown, formData: FormData) {
+  const response = await instance.post('/api/auth/signup', {
+    name: formData.get('name'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  if (response.data.success) {
+    console.log('Signup successful, user ID:', response.data.userId);
+  } else {
+    console.log('Signup error:', response.data.errors || response.data.error);
+    return response.data.errors
+  }
+}
 
 export default function Signup() {
   const [state, action, pending] = useActionState(signup, null)
